179ae3bbd8a3714097a6288f20d73b9c11e80889
Dadi Farisa <119612727+Dodolspartan@users.noreply.github.com>
2023-12-13 15:59:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import HomPages from "./pages/HomPages";
 import NavbarComponent from "./component/NavbarComponent";
@@ -13,27 +14,26 @@ import DoaComponent from "./component/DoaComponent";
 import BacaanSolatComponent from "./component/BacaanSolatComponent";
 import SurahDetailComponent from "./component/SurahDetailComponent";
 
-
-
 function App() {
   return (
     <div className="pages_element_alyp">
-    <div className="text-center">
-    <img className="element_image" src={Image} />
-    </div>
+      <div className="text-center">
+        {/* Add alt text for accessibility */}
+        <img className="element_image" src={Image} alt="Your Alt Text" />
+      </div>
       <NavbarComponent />
       <Routes>
         <Route path="/" element={<HomPages />} />
         <Route path="/favorite" element={<FavoritPages />} />
         <Route path="/setting" element={<SettingPages />} />
         <Route path="/tentangkami" element={<TentangKamiPages />} />
-        <Route path="/AlQuran" element={<AlQuranComponent/>}/>
-        <Route path="/AsmaulHusna" element={<AsmaulHusnaComponent/>} />
-        <Route path="/Wirid" element={<WiridComponent/>} />
-        <Route path="/Tahlil&Doa" element={<JuzAmmaComponent/>} />
-        <Route path="/Doa-Doa" element={<DoaComponent/>} />
-        <Route path="/SurahDetail/:nomor" element={<SurahDetailComponent/>} />
-        <Route path="/BacaanSholat" element={<BacaanSolatComponent/>} />
+        <Route path="/AlQuran" element={<AlQuranComponent />} />
+        <Route path="/AsmaulHusna" element={<AsmaulHusnaComponent />} />
+        <Route path="/Wirid" element={<WiridComponent />} />
+        <Route path="/Tahlil&Doa" element={<JuzAmmaComponent />} />
+        <Route path="/Doa-Doa" element={<DoaComponent />} />
+        <Route path="/SurahDetail/:nomor" element={<SurahDetailComponent />} />
+        <Route path="/BacaanSholat" element={<BacaanSolatComponent />} />
       </Routes>
     </div>
   );
