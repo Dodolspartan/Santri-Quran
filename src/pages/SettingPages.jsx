9576a594f9be126e84dac4f13bd5860d2e2b18b3
Dadi Farisa <119612727+Dodolspartan@users.noreply.github.com>
2024-04-14 04:05:31 +0700

    update v.01.0

--- src/pages/SettingPages.jsx
+++ src/pages/SettingPages.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import ListGroup from "react-bootstrap/ListGroup";
-import Form from "react-bootstrap/Form";
 import { Link } from "react-router-dom";
 import { toggleTheme } from "../disk/index"; // Make sure to import only what you need
-<<<<<<< HEAD
-import Image1 from "../assets/image/icon/Mask group.png";
-=======
->>>>>>> bf59c6eb43cf80a063d6e030abdc20e3541328f2
-
 function SettingPages() {
   // Add a key prop to each ListGroup.Item if you're rendering them dynamically
 
@@ -16,19 +10,6 @@ function SettingPages() {
       <ListGroup>
         <ListGroup.Item className="d-flex justify-content-between align-items-start">
           Mode Gelap
-          <div>
-            {/* Use onChange for Form.Check */}
-            <Form.Check
-              type="switch"
-              id="ChangeTheme"
-<<<<<<< HEAD
-              onChange={() => toggleTheme()}
-              disabled
-            />
-=======
-              onChange={() => toggleTheme()} disabled/>
->>>>>>> bf59c6eb43cf80a063d6e030abdc20e3541328f2
-          </div>
         </ListGroup.Item>
         <ListGroup.Item className="d-flex justify-content-between align-items-start">
           <Link className="link_element" to="/tentangkami">
